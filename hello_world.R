3ae949d73467d783ec52f4b94739024a5fb096af
Timothy Kornish <TimmehK@Timothys-MacBook-Pro.local>
2017-11-15 15:30:14 -0700

    Hello world program to start learning R with python for Machine Learning courses

---
+++ hello_world.R
@@ -0,0 +1,15 @@
+# My first program in R programming language
+
+# To run R script file in command line:
+
+# Rscript hello_world.R
+
+# output: [1] "Hello World"
+
+myString <- "Hello World"
+
+print(myString)
+
+myString2 = 'Hello'
+
+print(myString2)
